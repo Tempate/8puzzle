7c61424cd4f97981b4c91a60c743b850fdb74424
tempate <ddiazq28@gmail.com>
2021-05-05 14:24:37 +0200

    Readability updates

--- src/Board.hs
+++ src/Board.hs
@@ -9,7 +9,7 @@ type Tile = Maybe Int
 size = 3
 
 positions :: [Position]
-positions = [(m, n) | m <- [1..size], n <- [1..size]]
+positions = [(y, x) | y <- [1..size], x <- [1..size]]
 
 posOfTile :: Board -> Tile -> Position
 posOfTile board tile = head (filter hasTile positions)
@@ -18,16 +18,17 @@ posOfTile board tile = head (filter hasTile positions)
 tileAtPos :: Board -> Position -> Tile
 tileAtPos (T (_, f)) = f
 
-
+-- Two boards are equal if their tileAtPos method is equal
 instance Eq Board
   where b1 == b2 = all (\pos -> tileAtPos b1 pos == tileAtPos b2 pos) positions
 
+-- Pretty print a board
 instance Show Board
   where show board = "\n" ++ unlines (map (intercalate " | ") tilesByRow)
           where positionsByRow = groupBy (\p1 p2 -> fst p1 == fst p2) positions
                 tilesByRow = map (map (maybe " " show . tileAtPos board)) positionsByRow
 
-
+-- By default the goal board are tiles ordered left-to-right and top-to-bottom.
 goalBoard :: Board
 goalBoard = T ((size, size), tileAtPosGB)
 
@@ -36,15 +37,12 @@ tileAtPosGB (y, x)
     | x == size && y == size = Nothing
     | otherwise = Just ((y - 1) * size + x)
 
-
+-- Convert between boards and tiles
 tiles2Board :: [Tile] -> Board
 tiles2Board tiles = T (posOfTile auxBoard Nothing, tileAtPos)
   where auxBoard = T ((0, 0), tileAtPos)
-        tileAtPos = tileAtPosInList tiles
-
-tileAtPosInList :: [Tile] -> Position -> Tile
-tileAtPosInList tiles position = snd (head (filter ((== position).fst) tilesByPosition))
-  where tilesByPosition = zip positions tiles
+        tileAtPos = \pos -> snd (head (filter ((== pos) . fst) tilesByPos))
+        tilesByPos = zip positions tiles
 
 board2Tiles :: Board -> [Tile]
 board2Tiles board = map (tileAtPos board) positions
